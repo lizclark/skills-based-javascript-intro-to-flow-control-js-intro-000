fe29cc328f837b6914052b08a07514f482eb3dde
Liz Clark <ebclark917@gmail.com>
2021-05-03 18:18:14 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -20,5 +20,5 @@ function ageChecker(age) {
 }
 
 function ternaryTeenager (age) {
-  return age = (age>=13 %% age<=19) "You are a teenager" ; "You are not a teenager"
+  return (age>=13 && age<=19) "You are a teenager" ; "You are not a teenager"
 }

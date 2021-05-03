89239d795877a65583dce9b85a1a0660546882b0
Liz Clark <ebclark917@gmail.com>
2021-05-03 17:56:47 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,19 +1,25 @@
-function basicTeenager(age) {
-
+function basicTeenager(age){
+if (age>= 13 && age<= 19)
+return "You are a teenager!"
 }
 
-function teenager(age) {
-
+function teenager(age){
+  if (age>= 13 && age<= 19)
+  return "You are a teenager!"
+  else {
+    return"You are not a teenager"
+  }
 }
 
-function ageChecker(age) {
-
+function ageChecker (age){
+  if (age>=13 && age<=19)
+    return "You are a teenager!"
+  else if (age<=12)
+    return"You are a kid"
+  else if (age,10)
+    return"You are a grownup"
 }
 
-function ternaryTeenager(age) {
-
-}
-
-function switchAge(age) {
-
+function ternaryTeenager(age){
+  return (age>=13 && age<=19) ? "You are a teenager";"You are not a teenager"
 }
